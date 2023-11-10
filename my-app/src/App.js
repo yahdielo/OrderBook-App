f19bfefe3ed45d50a599bc9ffc5b133fb5011ce0
yahdielo <yahdielaz@gmail.com>
2023-11-10 12:40:08 -0400

    orderbook-bitstamp, added grid

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import OrderBook from "./OrderBook";
+
+const App = () => {
+  return (
+    <><div class="main">
+
+      <h2>Crypto Order Book </h2>
+      <OrderBook />
+    </div><div>
+        <h2>brrr</h2>
+      </div></>
+  );
+};
+
+export default App;
